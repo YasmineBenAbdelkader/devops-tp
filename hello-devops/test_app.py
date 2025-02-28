b71d28263717cdd813dd3a9bd679501c3caf7c14
yasmine <benabdelkaderyasmine2@gmail.com>
2025-02-28 22:57:20 +0100

    Add /status endpoint

--- hello-devops/test_app.py
+++ hello-devops/test_app.py
@@ -1,6 +1,11 @@
 import unittest
+from app import app
+
 class TestApp(unittest.TestCase):
-    def test_output(self):
-        self.assertTrue(True)
+    def test_home_route(self):
+        tester = app.test_client()
+        response = tester.get('/')
+        self.assertEqual(response.status_code, 200)
+
 if __name__ == "__main__":
     unittest.main()
